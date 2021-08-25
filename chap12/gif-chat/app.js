a650e2c78bc6a848d4e545d2530eaee04b99df27
왕종휘 <wangjh789@hanyang.ac.kr>
2021-08-25 12:44:28 +0900

    add chap12

--- chap12/gif-chat/app.js
+++ chap12/gif-chat/app.js
@@ -5,11 +5,12 @@ const cookieParser = require('cookie-parser')
 const session = require('express-session')
 const nunjucks = require('nunjucks')
 const dotenv = require('dotenv')
-
+const ColorHash = require('color-hash').default
 
 dotenv.config()
 const webSocket = require('./socket')
 const indexRouter = require('./routes')
+const connect = require('./schemas')
 
 const app = express()
 app.set('port',process.env.PORT || 8005)
@@ -18,21 +19,33 @@ nunjucks.configure('views',{
   express:app,
   watch:true
 })
+connect();
 
-app.use(morgan('dev'))
-app.use(express.static(path.join(__dirname,'public')))
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
-app.use(cookieParser(process.env.COOKIE_SECRET))
-app.use(session({
+const sessionMiddleware = session({
   resave:false,
   saveUninitialized:false,
-  secret:process.env.COOKIE_SECRET,
+  secret: process.env.COOKIE_SECRET,
   cookie:{
     httpOnly:true, //쿠키가 클라이언트 JavaScript가 아닌 HTTP(S)를 통해서만 전송되도록 함
     secure:false //브라우저가 HTTPS를 통해서만 쿠키를 전송하도록 합니다.
   }
-}))
+})
+
+app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname,'public')))
+app.use('/gif',express.static(path.join(__dirname,'uploads')))
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(cookieParser(process.env.COOKIE_SECRET))
+app.use(sessionMiddleware)
+
+app.use((req,res,next)=>{
+  if(!req.session.color){
+    const colorHash = new ColorHash()
+    req.session.color = colorHash.hex(req.sessionID)
+  }
+  next()
+})
 
 app.use('/',indexRouter)
 
@@ -53,4 +66,4 @@ const server = app.listen(app.get('port'),()=>{
   console.log(app.get('port'),'번 포트에서 대기 중')
 })
 
-webSocket(server);
+webSocket(server,app,sessionMiddleware);
